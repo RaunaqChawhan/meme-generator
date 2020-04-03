b4e45f77ff96292df49e0d8b47aebc2b5735e69c
New <chauhanraunaq878@gmail.com>
2020-04-03 12:31:08 +0530

    Get data and add text

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -4,12 +4,54 @@ class MemeGenerator extends Component {
 
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            topText: '',
+            bottomText: '',
+            randomImg: 'http://i.imgflip.com/1bij.jpg',
+            allMemeImgs: []
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(memesData => {
+                const { memes } = memesData.data;
+                this.setState({
+                    allMemeImgs: memes
+                })
+            })
     }
 
     render() {
         return (
-            <h1>From MemeGenerator Component</h1>
+            <div>
+                <form className="meme-form">
+                    <input
+                        type="text"
+                        name="topText"
+                        placeholder="Top Text"
+                        value={this.state.topText}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="bottomText"
+                        placeholder="Bottom Text"
+                        value={this.state.bottomText}
+                        onChange={this.handleChange}
+                    />
+                    <button type="submit">Gen</button>
+                </form>
+            </div>
         )
     }
 }
